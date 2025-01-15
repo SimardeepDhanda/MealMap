2697ffe2ef67e1a2574ae575377ce3e109028bef
SimardeepDhanda <146608721+SimardeepDhanda@users.noreply.github.com>
2025-01-15 15:52:59 -0500

    Create menuItemModel.js

---
+++ models/menuItemModel.js
@@ -0,0 +1,17 @@
+// models/menuItemModel.js
+const mongoose = require('mongoose');
+
+const menuItemSchema = new mongoose.Schema({
+  name: String,
+  calories: Number,
+  protein: Number,
+  carbs: Number,
+  fat: Number,
+  price: Number,
+  location: String,
+  date: Date,  // which day this menu item applies to
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('MenuItem', menuItemSchema);
